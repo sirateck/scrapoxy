f50bb315b3197d6cb36da370ece3d15807322df1
Kassim Belghait <kassim@sirateck.com>
2021-04-18 22:10:07 +0200

    Ping on mitmproxy

--- server/proxies/manager/instance.js
+++ server/proxies/manager/instance.js
@@ -35,7 +35,6 @@ module.exports = class Instance extends EventEmitter {
                 winston.debug('[Instance/%s] checkAlive every %d secs', self._model.name, self._config.checkAliveDelay);
                 self._checkAliveTimeout = setInterval(() => {
                     winston.debug('[Instance/%s] checkAlive: %s / %s', self._model.name, self._alive, self._aliveCount ? self._aliveCount : '-');
-
                     pinger.ping(self._model.address)
                         .then(() => {
                             self._changeAlive(true);
